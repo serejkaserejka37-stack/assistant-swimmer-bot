22e14880fee58806c6a874b65c1205d8c4240895
serejkaserejka37-stack <serejkaserejka37@gmail.com>
2025-11-11 09:57:29 +0300

    Delete init_database.py

--- init_database.py
+++
@@ -1,9 +0,0 @@
-from database.crud import init_db
-from analysis.recommendation_engine import init_recommendations
-
-if __name__ == '__main__':
-    print("Инициализация БД...")
-    init_db()
-    print("Добавление упражнений...")
-    init_recommendations()
-    print("✅ База данных готова!")
